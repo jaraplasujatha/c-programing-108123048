fbec5c7309eb1e792d73495d4b98aa6a95e1f300
jaraplasujatha <143472658+jaraplasujatha@users.noreply.github.com>
2023-09-12 17:11:53 +0530

    Week 1

---
+++ arthamtic oparetor.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+
+	  
+	int a,b,c,d,e,f ,g;
+printf("enter frist no");
+scanf("%d",&a);
+printf("enter second no"); 
+scanf("%d",&b);
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+g=a%b;
+printf("sum %d",c);
+
+printf( "difference %d\n",d);
+printf("multiply %d\n",e);
+printf(" modulus%d\n",g);
+printf("quotient%d",f);
+return 0;
+}
